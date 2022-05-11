03ad6ed928a9d8d4ee668daeedcfd3ef982b2bc2
Vidyoot Senthil <vidyoot@icloud.com>
2022-05-11 00:33:28 -0500

    main

--- V2/index.js
+++
@@ -1,6 +0,0 @@
-let img = document.querySelector("topImage")
-let start = img.src;
-let hover = "Light_Bulb_ON.png"
-
-img.onmouseover = () => { img.src = hover; }
-img.onmouseout = () => { img.src = start; } 
