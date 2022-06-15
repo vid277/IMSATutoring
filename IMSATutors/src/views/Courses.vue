9d9e4fffd3ce985b48fe1bbc9f1a6a9871a6f7b4
Vidyoot Senthil <vidyoot@icloud.com>
2022-06-14 23:54:34 -0500

    Started working on courses page

--- IMSATutors/src/views/Courses.vue
+++ IMSATutors/src/views/Courses.vue
@@ -28,7 +28,7 @@ export default {
 
 <template>
 <head>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;800&family=Nunito:wght@400;500&family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Nunito:wght@400;500;600;700;800&family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet">
     <link href="\src\assets\commons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -44,11 +44,166 @@ export default {
       <span id="bluesign" style="color: rgb(255, 255, 255)"><router-link to="/signup">Sign-Up</router-link></span>
     </header>
     <body>
-        <div class = "mainPageWrapper">
-            <div class = "innerBox">
-                <h2>Courses Offered:</h2>
+    <main>
+      <div id="wrapper">
+        <div class = "filterMenu">
+          <fieldset>
+            <h3><span>🗃️</span>Filter Menu</h3>
+            <h4>Search by Course:</h4>
+            <div class="search">
+              <form>
+                  <input type="text"
+                      placeholder="Type to start searching..."
+                      name="search"
+                      maxlength="20"     
+                  >
+              </form>
             </div>
+            <h4>Filter by Subject: </h4>
+            <div>
+                <div>
+                    <input type="checkbox" id="Math" name="Math">
+                    <label for="Math">Math</label>
+                </div> 
+                <div>
+                    <input type="checkbox" id="Science" name="Science">
+                    <label for="Science">Science</label>
+                </div> 
+                <div>
+                    <input type="checkbox" id="ComputerScience" name="ComputerScience">
+                    <label for="ComputerScience">Computer Science</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="Wellness" name="Wellness">
+                    <label for="Wellness">Wellness</label>
+                </div> 
+            </div>
+          </fieldset>
         </div>
+        <br>
+        <br>
+      </div>
+      <div class = "mainTextPlace">
+        <div class = "innerBox">
+          <div class = "TextPlaceWrapper">
+            <h2>Courses Offered: </h2>
+          </div>
+          <div class = "textDiv">
+            <h2>Math: </h2>
+            <div class = "courses mathCourses">
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">MI - I/II</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">MI - II</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">MI - III</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">MI - IV</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">BC - Calculus 1</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">BC - Calculus 2</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">BC - Calculus 3</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">Multivariable Calculus</h5>
+                </div> 
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">Number Theory</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">Linear Algebra</h5>
+                </div>
+            </div>
+          </div>
+          </div>
+          <div class = "textDiv" style = "background-color: rgba(235, 206, 232, 0.556);">
+            <h2>Science: </h2>
+            <div class = "courses scienceCourses">
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">SI - Chemistry</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">SI - Physics</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">MI - III</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">MI - IV</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">BC - Calculus 1</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">BC - Calculus 2</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">BC - Calculus 3</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">Multivariable Calculus</h5>
+                </div> 
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">Number Theory</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class = "innerCard">
+                    <h5 class="card-title">Linear Algebra</h5>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <br>
+        <br>
+        <br>
+      </div>
+    </main>
     </body>
 </div>
 <div v-else class = "mobileMessage">
@@ -69,26 +224,167 @@ export default {
 
 #indexheader {
   position: relative !important;
+  text-align: center;
 }
 
-.mainPageWrapper {
+main {
+  margin-top:  0px !important;
+  padding: 0% !important;
+  width: 100%;
+  height: 100%;
+  gap: 0rem !important;
+}
+
+#wrapper {
+  height: calc(100vh);
+  border-right: 2px solid rgb(203, 213, 224);
+  overflow-y: auto !important;
+  width: 25%;
+}
+
+.filterMenu {
+  padding: 25px;
+}
+
+.filterMenu h3 {
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  border-bottom: solid 2px #69696929;
+  text-align: center;
+}
+
+.filterMenu h4 {
+  font-weight: bold;
+  margin-top: 13px;
+}
+
+.filterMenu h5 {
+  margin-top: 5px;
+  margin-left: 10px;
+  font-size: 14.5px;
+  font-weight: 700;
+  text-decoration: underline;
+}
+
+#wrapper .filterMenu fieldset{
+  border: solid 0px;
+  justify-content: flex-start;
+  height: 100%;
+}
+
+#wrapper .filterMenu div input {
+  margin: 0.6rem;
+}
+
+#wrapper .filterMenu div label {
+  margin: 0.6rem;
+}
+
+.search input[type=text]{
+  height: 35px;
+  border-radius: 5px;
+  border: solid 1px;
+  padding: 5px;
+  font-size: small;
+  width: 100%;
+}
+
+.search input{
+  margin: 0% !important;
+}
+
+.search {
+  margin-top: 10px;
+}
+
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(203, 213, 224);
+}
+
+.mainTextPlace{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+  height: calc(100vh);
+  width: 100%;
+}
+
+.mainTextPlace .innerBox {
+  width: 100%;
+  padding: 2%;
+  height: 100%;
+  overflow-y: auto;
+}
+
+.TextPlaceWrapper h2{
+    font-weight: 700;
+    background-color: rgba(203, 213, 224, 0.24);
+    border-radius: 4px;
+    padding: 1.75%;
+}
+
+.courses {
+    display: flex;
+    gap: 0.5em;
+    row-gap: 0.5em;
+    flex-wrap: wrap;
+}
+
+.card {
+  padding: 0em 1em 0em ;
+  border-radius: 25px;
+  text-align: center;
+  border: solid 2px;
+  border-color: rgb(203, 213, 224);
+  background-color: rgb(244, 241, 241);
+  display: flex;
+  height: fit-content;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  row-gap: 0.25em;
+  transition: 0.3s;
+}
+
+.card .innerCard {
     width: 100%;
-    height: 100%;
+    margin: 20px;
 }
 
-.innerBox{
-    margin: 1%;
-    width: 100%;
-    height: 100%;
+.card .innerCard h5{
+    font-size: medium;
 }
 
-.innerBox h2 {
-    font-weight: 500;
-    font-size: 25px;
-    border-bottom: solid 2px;
-    max-width: fit-content;
-    padding: 0%;
-    margin: 0%;
+.card .card-title {
+  font-weight: 700;
+  font-size: 1.5em;
 }
 
+.card:hover {
+  background-color: #69696929;
+  color: #000000;
+}
+
+.textDiv {
+    padding: 1%;
+    background-color: rgb(153, 189, 227, 0.5);
+    margin-top: 1%;
+    border-radius: 5px;
+}
+
+.textDiv h2 {
+    font-weight: 700;
+    font-size: 20px;
+    text-decoration: underline;
+    padding: 0.5%;
+    margin-bottom: 0.5%;
+    width: max-content;
+}
 </style>
