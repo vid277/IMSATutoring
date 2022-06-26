b4ec41bf923b4c303c5e6073c9e50a13b4997a52
Sahil Veeravalli <214lksss@gmail.com>
2022-06-26 18:08:34 -0500

    Homepage

--- client/src/pages/Homepage.vue
+++ client/src/pages/Homepage.vue
@@ -6,8 +6,16 @@
 </script>
 
 <template>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;800&family=Nunito:wght@400;500&family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet">
+<link href="\src\assets\commons.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <main>
+    <div class = "container container1"></div>
+    <div class = "container container2"></div>
+    <div class = "container container3"></div>
 </main>
+
 </template>
 
 <style scoped>
@@ -16,15 +24,33 @@
   box-sizing: border-box;
   margin: 0%;
   overflow-x: hidden;
+  overflow-y: hidden;
   padding: 0%;
 }
-
-main {
-    margin-top:  0px !important;
-    padding: 0% !important;
-    width: 100%;
+main 
+{
+    display:flex;
+    flex-direction: column;
     height: 100%;
-    gap: 0rem !important;
+    width: 100%;
 }
-
+.container{
+    height: 100%;
+    width: 100%;
+}
+.container1{
+    display: flex;
+    flex-direction: row;
+    background-image: url(.../assets/HomePage1.svg);
+}
+.container2{
+    display: flex;
+    flex-direction: row;
+    background-image: url(.../assets/HomePage2.svg);
+}
+.container3{
+    display: flex;
+    flex-direction: row;
+    background-image: url(.../assets/HomePage3.svg);
+} 
 </style>
